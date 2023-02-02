94e356db0253da38277e8bd30308bf936979886a
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-02-02 05:15:53 -0800

    Setup db schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department VALUES
+(null, "IT"),
+(null, "Human Resources"),
+(null, "Operations"),
+(null, "Accounting"),
+(null, "Engineering"),
+(null, "Professional Services");
+
+INSERT INTO role VALUES
+(null, "Server Admin", 80000, 1),
+(null, "Tech Recruiter", 75000, 2),
+(null, "Bean Counter", 50000, 4),
+(null, "Engineering Manager", 400000, 5),
+(null, "Engineer", 250000, 5);
+
+INSERT INTO employee VALUES
+(null, "Bandy", "Dandy", 2, null),
+(null, "Candy", "Flandy", 1, null),
+(null, "Sandy", "Zandy", 4, null),
+(null, "Tandy", "Blandy", 5, 3);
